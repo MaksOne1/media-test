422b21de98df348bd51b0833491f294603bfc8c9
makli <mak.lis2016@gmail.com>
2024-04-21 14:20:47 +0300

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,22 @@
 'use client'
-import ReactPlayer from 'react-player'
 import React from "react";
-import {Media, Video } from '@vidstack/player-react';
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import {defaultLayoutIcons, DefaultVideoLayout} from "@vidstack/react/player/layouts/default";
 
 export default function Page() {
   return <div>
-    mkv
-    <Media>
-      <Video loading="visible" controls preload="true">
-        <video loading="visible" src="http://92.246.136.177/YLHjtVpwG1NrWLAVng0wCRSIuV3dmX.mkv" preload="none" data-video="0" controls />
-      </Video>
-    </Media>
-    mp4
-    <Media>
-      <Video loading="visible" controls preload="true">
-        <video loading="visible" src="http://92.246.136.177/kTsSHuNQ40YfmuEEbOer7RXLSkDTMB.mp4" preload="none" data-video="0" controls />
-      </Video>
-    </Media>
-    mkv2
-    <Media>
-      <Video loading="visible" controls preload="true">
-        <video loading="visible" src="http://92.246.136.177/0Nd7uoIqOi9dmlPmXkECADKK1eOinH.mkv" preload="none" data-video="0" controls />
-      </Video>
-    </Media>
-  </div>
+      <MediaPlayer title="Sprite Fight" src="http://92.246.136.177/0Nd7uoIqOi9dmlPmXkECADKK1eOinH.mp4">
+      <MediaProvider />
+      <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} />
+</MediaPlayer>
+  <MediaPlayer title="Sprite Fight" src="http://92.246.136.177/kTsSHuNQ40YfmuEEbOer7RXLSkDTMB.mp4">
+    <MediaProvider />
+    <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} />
+  </MediaPlayer>
+  <MediaPlayer title="Sprite Fight" src="http://92.246.136.177/0Nd7uoIqOi9dmlPmXkECADKK1eOinH.mp4">
+    <MediaProvider />
+    <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} />
+  </MediaPlayer>
+</div>
+
 }
